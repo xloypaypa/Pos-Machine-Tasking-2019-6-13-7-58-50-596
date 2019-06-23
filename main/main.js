02f8c3708e90ea5a8b8d0c7693176c4982ea46b8
suxin <xloypaypa@gmail.com>
2019-06-23 17:02:17 +0800

    can get item count

--- main/main.js
+++ main/main.js
@@ -8,3 +8,22 @@ function loadAllItemMap() {
     }
     return result;
 }
+
+function getItemCount(tags) {
+    const result = [];
+    for (let i = 0; i < tags.length; i++) {
+        const item = tags[i];
+        let exist = false;
+        for (let j = 0; j < result.length; j++) {
+            if (result[j].key === item) {
+                result[j].count ++;
+                exist = true;
+                break;
+            }
+        }
+        if (!exist) {
+            result.push({key: item, count: 1});
+        }
+    }
+    return result;
+}
